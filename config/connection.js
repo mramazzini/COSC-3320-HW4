e362ed606f6ec24fd29e837954e913130f87ae60
mramazzini <mramazzini123@gmail.com>
2024-11-06 19:45:17 -0600

    Added postgres str

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,17 @@ const postgres = require("postgres");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const sql = postgres({
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-});
+let sql;
 
+if (process.env.DATABASE_URL) {
+  sql = postgres(process.env.DATABASE_URL);
+} else {
+  sql = postgres({
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+}
 module.exports = { sql };
